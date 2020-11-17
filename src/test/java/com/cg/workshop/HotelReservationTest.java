fa22d8dc331be1d82f0e0ae2847608b0cbdc249d
Bibhav Singh <singhbibhav084@gmail.com>
2020-11-17 21:38:35 +0530

    finding cheapest best rated hotel in given date range by java streams

--- src/test/java/com/cg/workshop/HotelReservationTest.java
+++ src/test/java/com/cg/workshop/HotelReservationTest.java
@@ -1,11 +1,7 @@
 package com.cg.workshop;
 
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
-
+import org.junit.*;
 
 public class HotelReservationTest {
     HotelReservationMain hotelReservationMain;
